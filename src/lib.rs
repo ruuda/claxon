9caddd1a4df018671d1303917205361b04f1f327
Ruud van Asseldonk <dev@veniogames.com>
2014-12-20 20:47:30 +0100

    move errors into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,12 @@
 #![allow(dead_code)]
 
-use std::error::FromError;
-use std::io::IoError;
+use error::FlacError;
 
-struct Frame;
-
-#[deriving(Show)]
-pub enum FlacError {
-    /// Not a decoding error, but a problem with the underlying IO.
-    IoError(IoError),
-
-    /// The stream header does not equal 'fLaC'.
-    InvalidStreamHeader,
-
-    /// Metadata block type 127 is invalid, to avoid confusion with a frame sync code.
-    InvalidMetadataBlockType,
-    /// This version of the library cannot handle this kind of metadata block,
-    /// it was reserved at the time of writing.
-    ReservedMetadataBlockType,
-
-    /// A lower bound was encountered that was bigger than an upper bound.
-    InconsistentBounds,
-    /// The minimum block size must be larger than 15.
-    InvalidBlockSize,
-    /// The sample rate must be positive and no larger than 6553550 Hz.
-    InvalidSampleRate,
-
-    /// The streaminfo block must be the very first metadata block.
-    MissingStreamInfoBlock,
-}
+mod error;
 
 pub type FlacResult<T> = Result<T, FlacError>;
 
-// TODO: implement the Error trait for FlacError.
-
-impl FromError<IoError> for FlacError {
-    fn from_error(err: IoError) -> FlacError {
-        FlacError::IoError(err)
-    }
-}
+struct Frame;
 
 struct FlacStream {
     streaminfo: StreamInfo,
