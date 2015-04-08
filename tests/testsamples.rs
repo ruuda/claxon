da5146ec68d67a797c75418684cde34a0bd53b52
Ruud van Asseldonk <dev@veniogames.com>
2015-04-09 01:26:52 +0200

    add test that compares decoding with the reference decoder

--- tests/testsamples.rs
+++ tests/testsamples.rs
@@ -16,6 +16,7 @@
 #![feature(path_ext)]
 
 extern crate claxon;
+extern crate hound;
 
 use std::fs;
 use std::io;
@@ -38,6 +39,18 @@ fn run_metaflac(fname: &path::Path) -> String {
                          .arg(fname.to_str().expect("unsupported filename"))
                          .output().ok().expect("failed to run metaflac");
     String::from_utf8(output.stdout).ok().expect("metaflac wrote invalid UTF-8")
+}
+
+fn decode_file(fname: &path::Path) {
+    use std::process::Command;
+
+    // Run the the reference flac decoder on the file.
+    let success = Command::new("flac")
+                          .arg("--decode")
+                          .arg(fname.to_str().expect("unsupported filename"))
+                          .status().ok().expect("failed to run flac")
+                          .success();
+    assert!(success);
 }
 
 fn print_hex(seq: &[u8]) -> String {
@@ -81,6 +94,65 @@ fn compare_metaflac(fname: &path::Path) {
     };
 }
 
+fn compare_decoded_stream(fname: &path::Path) {
+    use std::fs::PathExt;
+
+    let ref_fname = fname.with_extension("wav");
+    if !ref_fname.exists() {
+        // TODO: actually, we might only want to run the test for files that
+        // do exist already. It is fine to run streaminfo or decode-only tests
+        // on thousands of files, but decoding thousands of files to wav before
+        // running the tests will take a lot of space. For now, while the
+        // number of samples is closer to 10 than to 100, this is fine.
+        decode_file(fname);
+    }
+
+    // If the reference file does exist after decoding, we can compare it to
+    // how Claxon decodes it, sample by sample.
+    if ref_fname.exists() {
+        let ref_file = fs::File::open(ref_fname).unwrap();
+        let ref_reader = io::BufReader::new(ref_file);
+        let mut ref_stream = hound::WavReader::new(ref_reader).unwrap();
+
+        let try_file = fs::File::open(fname).unwrap();
+        let mut try_reader = io::BufReader::new(try_file);
+        // TODO: Claxon should take R by value, not by mut reference.
+        let mut try_stream = claxon::FlacStream::new(&mut try_reader).unwrap();
+
+        // The streaminfo test will ensure that things like bit depth and
+        // sample rate match, only the actual samples are compared here.
+
+        let mut ref_samples = ref_stream.samples::<i16>();
+
+        let n_samples = try_stream.streaminfo().n_samples.unwrap();
+        let n_channels = try_stream.streaminfo().n_channels;
+        // TODO: use i32 to be generic (when Hound supports this).
+        let mut blocks = try_stream.blocks::<i16>();
+        let mut sample = 0u64;
+        let mut b = 0u64;
+
+        while sample < n_samples {
+            let block = blocks.read_next().unwrap();
+            let mut channels: Vec<_> = (0 .. n_channels)
+                                       .map(|i| block.channel(i).iter().cloned())
+                                       .collect();
+            for i in (0 .. block.len()) {
+                for ch in (0 .. n_channels as usize) {
+                    let ref_sample = ref_samples.next().map(|r| r.ok().unwrap());
+                    let try_sample = channels[ch].next();
+                    if ref_sample != try_sample {
+                        println!("disagreement at sample {} of block {} in channel {}: reference is {} but decoded is {}",
+                                 i, b, ch, ref_sample.unwrap(), try_sample.unwrap());
+                        panic!("decoding differs from reference decoder");
+                    }
+                }
+            }
+            sample = sample + block.len() as u64;
+            b = b + 1;
+        }
+    }
+}
+
 fn for_each_test_sample<F: Fn(&path::Path)>(f: F) {
     use std::ffi::OsStr;
     use std::fs::PathExt;
@@ -103,4 +175,9 @@ fn for_each_test_sample<F: Fn(&path::Path)>(f: F) {
 #[test]
 fn verify_streaminfo() {
     for_each_test_sample(compare_metaflac);
+}
+
+#[test]
+fn verify_decoded_stream() {
+    for_each_test_sample(compare_decoded_stream);
 }
