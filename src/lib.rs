b1c96795873d1c4eea51dd5ad6e4f057214ba579
Ruud van Asseldonk <dev@veniogames.com>
2015-07-13 23:59:58 +0200

    use new name for unstable feature

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 
 #![warn(missing_docs)]
 #![allow(dead_code)] // TODO: Remove for v0.2
-#![feature(core, zero_one)]
+#![feature(iter_arith, zero_one)]
 
 use std::io;
 use error::{Error, FlacResult};
