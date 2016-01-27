28c1f3e72dee0544029ab1194ae893fa76205c8c
Ruud van Asseldonk <dev@veniogames.com>
2016-01-27 10:40:44 +0100

    mention only first year in copyright headers
    It is too tedious to keep updating the headers, and to forget to do so
    all the time.

--- src/sample.rs
+++ src/sample.rs
@@ -1,5 +1,5 @@
 // Claxon -- A FLAC decoding library in Rust
-// Copyright (C) 2015 Ruud van Asseldonk
+// Copyright 2015 Ruud van Asseldonk
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License, version 3,
