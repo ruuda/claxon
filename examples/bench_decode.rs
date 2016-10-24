84fd18368ab3b373f5b6c71d665e70294f56e452
Ruud van Asseldonk <dev@veniogames.com>
2016-10-24 17:13:04 +0200

    Avoid using the unstable "test" crate
    Claxon was compiling on stable (well, beta) Rust, and I'd like to keep
    it that way if I can. It turns out the black box does very little here,
    so it can be removed.
    
    Also generalize the benchmark over bit depth.

--- examples/bench_decode.rs
+++ examples/bench_decode.rs
@@ -5,17 +5,15 @@
 // you may not use this file except in compliance with the License.
 // A copy of the License has been included in the root of the repository.
 
-#![feature(test)]
-
 extern crate claxon;
-extern crate test;
 extern crate time;
 
+use claxon::FlacReader;
+use claxon::sample::Sample;
 use std::env;
-use std::path::Path;
 use std::fs::File;
 use std::io::{Cursor, Read};
-use claxon::FlacReader;
+use std::path::Path;
 use time::PreciseTime;
 
 /// Reads a file into memory entirely.
@@ -26,17 +24,19 @@ fn read_file<P: AsRef<Path>>(path: P) -> Vec<u8> {
     data
 }
 
-/// Decode a file into 16-bit integers.
+/// Decode a file into 16-bit or 32-bit integers.
 ///
-/// This consumes the decoded samples into a black box. For every block decoded,
-/// appends to the vector the average time spent per sample in nanoseconds.
+/// For every block decoded, appends to the vector the average time spent per
+/// sample for that block in nanoseconds.
 ///
 /// Returns a pair of (total time / total samples, total bytes / total time) in
 /// units (nanoseconds, bytes per second). Different channels account for
 /// different samples; a stereo file of the same sample rate and duration as a
 /// mono file will have twice as many samples. Bytes refers to the number of
 /// input bytes. (TODO: Do not count header bytes there.)
-fn decode_file_i16(data: &[u8], sample_times_ns: &mut Vec<i32>) -> (i64, i64) {
+fn decode_file<S: Sample>(data: &[u8],
+                          sample_times_ns: &mut Vec<i32>)
+                          -> (i64, i64) {
     let cursor = Cursor::new(data);
     let mut reader = FlacReader::new(cursor).unwrap();
 
@@ -45,7 +45,7 @@ fn decode_file_i16(data: &[u8], sample_times_ns: &mut Vec<i32>) -> (i64, i64) {
     assert!(bps < 8 * 16);
 
     let mut sample_buffer = Vec::new();
-    let mut frame_reader = reader.blocks::<i16>();
+    let mut frame_reader = reader.blocks::<S>();
     let mut frame_epoch = PreciseTime::now();
     let epoch = frame_epoch;
 
@@ -59,9 +59,12 @@ fn decode_file_i16(data: &[u8], sample_times_ns: &mut Vec<i32>) -> (i64, i64) {
                 sample_times_ns.push(duration_ns as i32 / num_samples as i32);
                 frame_epoch = now;
 
-                // Recycle the buffer for the next frame. Pull it through a
-                // black box to ensure that decoding is not optimized away.
-                sample_buffer = test::black_box(block.into_buffer());
+                // Recycle the buffer for the next frame. There should be a
+                // black box to prevent optimizing this away, but it is only
+                // available on unstable Rust, and it seems that nothing is
+                // optimized away here anyway. The decode needs to happen anyway
+                // to decide whether we hit the panic below.
+                sample_buffer = block.into_buffer();
             },
             Ok(None) => break, // End of file.
             Err(_) => panic!("failed to decode")
@@ -72,21 +75,6 @@ fn decode_file_i16(data: &[u8], sample_times_ns: &mut Vec<i32>) -> (i64, i64) {
     let ns_per_sample = total_duration_ns / num_samples;
     let bytes_per_sec = data.len() as i64 * 1000_000_000 / total_duration_ns;
     (ns_per_sample, bytes_per_sec)
-}
-
-/// Decode a file into 16-bit integers.
-///
-/// This consumes the decoded samples into a black box.
-fn decode_file_i32(data: &[u8]) {
-    let cursor = Cursor::new(data);
-    let mut reader = FlacReader::new(cursor).unwrap();
-
-    let bps = reader.streaminfo().bits_per_sample as u64;
-    assert!(bps < 8 * 16);
-
-    for sample in reader.samples::<i32>() {
-        test::black_box(sample.unwrap());
-    }
 }
 
 fn print_stats(sample_times_ns: &mut Vec<i32>, stats_pair: (i64, i64)) {
@@ -109,11 +97,13 @@ fn main() {
 
     if bits == "16" {
         // TODO: Do several passes and report timing information.
-        let stats_pair = decode_file_i16(&data, &mut sample_times_ns);
+        let stats_pair = decode_file::<i16>(&data, &mut sample_times_ns);
         print_stats(&mut sample_times_ns, stats_pair);
         sample_times_ns.clear();
     } else if bits == "32" {
-        decode_file_i32(&data);
+        let stats_pair = decode_file::<i32>(&data, &mut sample_times_ns);
+        print_stats(&mut sample_times_ns, stats_pair);
+        sample_times_ns.clear();
     } else {
         panic!("expected bit depth of 16 or 32");
     }
