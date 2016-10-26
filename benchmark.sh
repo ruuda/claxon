cd933aa82df2803ab767b1364e076252acab32b3
Ruud van Asseldonk <dev@veniogames.com>
2016-10-26 14:08:33 +0200

    Update benchmark script for new fixed sample width

--- benchmark.sh
+++ benchmark.sh
@@ -23,21 +23,16 @@ cargo build --verbose --release --example bench_decode
 # Run the benchmarks with "taskset" to lock them to the same CPU core for the
 # entire program, to lower variance in the measurements.
 echo "Benchmarking sample file 0 ..."
-taskset -c 1 target/release/examples/bench_decode 16 testsamples/b0_daft_punk_one_more_time.flac > "$1_b0_16.dat"
-taskset -c 1 target/release/examples/bench_decode 32 testsamples/b0_daft_punk_one_more_time.flac > "$1_b0_32.dat"
+taskset -c 1 target/release/examples/bench_decode testsamples/b0_daft_punk_one_more_time.flac > "$1_b0.dat"
 
 echo "Benchmarking sample file 1 ..."
-taskset -c 1 target/release/examples/bench_decode 16 testsamples/b1_deadmau5_i_remember.flac > "$1_b1_16.dat"
-taskset -c 1 target/release/examples/bench_decode 32 testsamples/b1_deadmau5_i_remember.flac > "$1_b1_32.dat"
+taskset -c 1 target/release/examples/bench_decode testsamples/b1_deadmau5_i_remember.flac > "$1_b1.dat"
 
 echo "Benchmarking sample file 2 ..."
-taskset -c 1 target/release/examples/bench_decode 16 testsamples/b2_massive_attack_unfinished_sympathy.flac > "$1_b2_16.dat"
-taskset -c 1 target/release/examples/bench_decode 32 testsamples/b2_massive_attack_unfinished_sympathy.flac > "$1_b2_32.dat"
+taskset -c 1 target/release/examples/bench_decode testsamples/b2_massive_attack_unfinished_sympathy.flac > "$1_b2.dat"
 
 echo "Benchmarking sample file 3 ..."
-taskset -c 1 target/release/examples/bench_decode 16 testsamples/b3_muse_starlight.flac > "$1_b3_16.dat"
-taskset -c 1 target/release/examples/bench_decode 32 testsamples/b3_muse_starlight.flac > "$1_b3_32.dat"
+taskset -c 1 target/release/examples/bench_decode testsamples/b3_muse_starlight.flac > "$1_b3.dat"
 
 echo "Benchmarking sample file 4 ..."
-taskset -c 1 target/release/examples/bench_decode 16 testsamples/b4_u2_sunday_bloody_sunday.flac > "$1_b4_16.dat"
-taskset -c 1 target/release/examples/bench_decode 32 testsamples/b4_u2_sunday_bloody_sunday.flac > "$1_b4_32.dat"
+taskset -c 1 target/release/examples/bench_decode testsamples/b4_u2_sunday_bloody_sunday.flac > "$1_b4.dat"
