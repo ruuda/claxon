5b477743346f828b2111846802493bce73d3a2df
Ruud van Asseldonk <dev@veniogames.com>
2016-11-02 00:41:15 +0100

    Load entire file into memory for decode example
    This speeds things up a little bit.

--- examples/decode.rs
+++ examples/decode.rs
@@ -8,15 +8,27 @@
 extern crate claxon;
 extern crate hound;
 
-fn main() {
-    use std::env;
-    use std::path;
-    use claxon::FlacReader;
-    use hound::{WavSpec, WavWriter};
+use claxon::FlacReader;
+use hound::{WavSpec, WavWriter};
+use std::env;
+use std::fs::File;
+use std::io::{Cursor, Read};
+use std::path;
 
+fn main() {
     let arg = env::args().nth(1).expect("no file given");
     let fname = path::Path::new(&arg);
-    let mut reader = FlacReader::open(fname).expect("failed to open FLAC stream");
+    let mut file = File::open(fname).expect("failed to open FLAC file");
+
+    // Load the entire file into memory at once. This allows using a cursor
+    // afterwards to read the data, which is cheaper than mixing IO with the
+    // actual FLAC reading due to inlining.
+    let mut bytes = Vec::new();
+    bytes.reserve(file.metadata().unwrap().len() as usize);
+    file.read_to_end(&mut bytes).expect("failed to read FLAC data into memory");
+
+    let data = Cursor::new(bytes);
+    let mut reader = FlacReader::new(data).expect("failed to open FLAC stream");
 
     let spec = WavSpec {
         // TODO: u8 for channels, is that weird? Would u32 be better?
