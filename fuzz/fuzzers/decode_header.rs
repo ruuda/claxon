8b796baeb65dc5edcc8e8a17ae1e9f6bf615a0e8
Ruud van Asseldonk <dev@veniogames.com>
2017-05-22 19:56:09 +0200

    Add two smaller fuzzing targets
    One decodes only the header. The idea is that the corpus for this one
    can be used as input to the other stages. Only reading the header should
    be faster than reading the entire file.
    
    The other target decodes a single block. For large files this could make
    sure that no time is wasted on first decoding blocks which are valid
    anyway; this should force the fuzzer to discover interesting samples
    sooner.

---
+++ fuzz/fuzzers/decode_header.rs
@@ -0,0 +1,22 @@
+// Claxon -- A FLAC decoding library in Rust
+// Copyright 2017 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
+#![no_main]
+
+extern crate libfuzzer_sys;
+extern crate claxon;
+
+use std::io::Cursor;
+
+#[export_name="rust_fuzzer_test_input"]
+pub extern fn go(data: &[u8]) {
+    let cursor = Cursor::new(data);
+    match claxon::FlacReader::new(cursor) {
+        Ok(..) => {}
+        Err(..) => {}
+    };
+}
---
+++ fuzz/fuzzers/decode_single_block.rs
@@ -0,0 +1,27 @@
+// Claxon -- A FLAC decoding library in Rust
+// Copyright 2017 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
+#![no_main]
+
+extern crate libfuzzer_sys;
+extern crate claxon;
+
+use std::io::Cursor;
+
+#[export_name="rust_fuzzer_test_input"]
+pub extern fn go(data: &[u8]) {
+    let cursor = Cursor::new(data);
+    let mut reader = match claxon::FlacReader::new(cursor) {
+        Ok(r) => r,
+        Err(..) => return,
+    };
+
+    match reader.blocks().read_next_or_eof(Vec::new()) {
+        Ok(..) => { }
+        Err(..) => {}
+    }
+}
