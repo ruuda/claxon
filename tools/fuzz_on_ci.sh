e8b472abe8c01b8a27c3aa8e90720d07d8fd4a55
Ruud van Asseldonk <dev@veniogames.com>
2017-05-27 17:13:44 +0200

    Tweak fuzzing on CI

--- tools/fuzz_on_ci.sh
+++ tools/fuzz_on_ci.sh
@@ -8,7 +8,7 @@ if [[ "${TRAVIS_RUST_VERSION}" != "nightly" ]]; then
   exit 0
 fi
 
-cargo install cargo-fuzz --vers 0.3.1 || true
+cargo install cargo-fuzz --vers 0.4.2 || true
 
 # Pre-populate the corpus with the test samples, if they did not exist already.
 # Note that we do not cache the corpus directly on Travis, due to this bug:
@@ -17,9 +17,9 @@ mkdir -p fuzz/corpus
 mkdir -p fuzz_corpus
 cp --no-clobber testsamples/*.flac fuzz_corpus
 cp --no-clobber testsamples/fuzz/*.flac fuzz_corpus
-cp --no-clobber fuzz_corpus/* fuzz/corpus
+cp --no-clobber fuzz_corpus/* fuzz/corpus/decode_full
 
-echo "Corpus size: $(ls -A fuzz/corpus | wc -l)"
+echo "Corpus size: $(ls -A fuzz/corpus/decode_full | wc -l)"
 
 echo "Running fuzzer for ${FUZZ_SECONDS:-10} seconds ..."
 
@@ -29,13 +29,13 @@ echo "Running fuzzer for ${FUZZ_SECONDS:-10} seconds ..."
 # actually everything is fine.
 export ASAN_OPTIONS="detect_leaks=0"
 
-# Set max length to a small-ish number (in comparison to the test samples), as
-# the coverage is similar (it is a lot harder to find a few elusive paths), but
-# every iteration runs much faster. Warn about slow runs, as every iteration
-# should execute in well below a second. Disable the leak sanitizer, otherwise
-# it reports a leak when the fuzzer exits after the given total time.
+# Set max length to the size of a minimal flac file, as the coverage is similar
+# (it is a lot harder to find a few elusive paths), but every iteration runs
+# much faster. Warn about slow runs, as every iteration should execute in well
+# below a second. Disable the leak sanitizer, otherwise it reports a leak when
+# the fuzzer exits after the given total time.
 cargo fuzz run decode_full -- \
-  -max_len=2048 \
+  -max_len=8192 \
   -report_slow_units=1 \
   -max_total_time=${FUZZ_SECONDS:-10} \
   -print_final_stats=1 \
@@ -44,4 +44,4 @@ cargo fuzz run decode_full -- \
 # Copy back any new discoveries, so Travis can cache them. This step is not
 # reached when fuzzing finds a crash, but that is ok, because in that case we
 # should reproduce manually and add a regression test anyway.
-cp --no-clobber fuzz/corpus/* fuzz_corpus
+cp --no-clobber fuzz/corpus/decode_full/* fuzz_corpus
