242d0459fe22c05a5339dd2f381ca87207f2e137
Ruud van Asseldonk <dev@veniogames.com>
2018-09-17 20:44:42 +0200

    Make bench_decode output sample times only
    The benchmark program should not try to be smart and do statistics, we
    will just output the raw time per sample (still averaged over the block,
    as we need to decode block by block), and leave analysis to R
    afterwards.

--- examples/bench_decode.rs
+++ examples/bench_decode.rs
@@ -33,23 +33,11 @@ fn read_file<P: AsRef<Path>>(path: P) -> Vec<u8> {
 /// different samples; a stereo file of the same sample rate and duration as a
 /// mono file will have twice as many samples. Bytes refers to the number of
 /// input bytes.
-fn decode_file(data: &[u8], sample_times_ns: &mut Vec<f64>) -> (f64, f64) {
-    // To get the real number of data bytes (excluding metadata -- which can be
-    // quite big due to album art), construct a new reader around a cursor, but
-    // extract the cursor immediately afterwards. This will read the metadata
-    // but not yet any audio data. The position of the cursor is then the amount
-    // of metadata bytes.
-    let data_bytes = data.len() - FlacReader::new(Cursor::new(data))
-        .unwrap()
-        .into_inner()
-        .position() as usize;
-
-    let cursor = Cursor::new(data);
-    let mut reader = FlacReader::new(cursor).unwrap();
+fn decode_file(data: &[u8], sample_times_ns: &mut Vec<f64>) {
+    let mut reader = FlacReader::new(Cursor::new(data)).unwrap();
 
     let bps = reader.streaminfo().bits_per_sample as u64;
     let num_channels = reader.streaminfo().channels;
-    let num_samples = reader.streaminfo().samples.unwrap() as i64 * num_channels as i64;
     assert!(bps < 8 * 16);
 
     // Allocate a buffer once that is big enough to fit all the blocks (after
@@ -59,7 +47,6 @@ fn decode_file(data: &[u8], sample_times_ns: &mut Vec<f64>) -> (f64, f64) {
 
     let mut frame_reader = reader.blocks();
     let mut frame_epoch = PreciseTime::now();
-    let epoch = frame_epoch;
 
     loop {
         match frame_reader.read_next_or_eof(sample_buffer) {
@@ -77,39 +64,25 @@ fn decode_file(data: &[u8], sample_times_ns: &mut Vec<f64>) -> (f64, f64) {
                 // to decide whether we hit the panic below.
                 sample_buffer = block.into_buffer();
             },
-            Ok(None) => break, // End of file.
+            Ok(None) => return, // End of file.
             Err(_) => panic!("failed to decode")
         }
     }
-
-    let total_duration_ns = epoch.to(PreciseTime::now()).num_nanoseconds().unwrap();
-    let ns_per_sample = total_duration_ns as f64 / num_samples as f64;
-    let bytes_per_sec = data_bytes as f64 * 1000_000_000.0 / total_duration_ns as f64;
-    (ns_per_sample, bytes_per_sec)
-}
-
-fn print_stats(sample_times_ns: &mut Vec<f64>, stats_pair: (f64, f64)) {
-    let (ns_per_sample, bytes_per_sec) = stats_pair;
-    sample_times_ns.sort_by(|x, y| x.partial_cmp(y).unwrap());
-
-    let p10 = sample_times_ns[10 * sample_times_ns.len() / 100];
-    let p50 = sample_times_ns[50 * sample_times_ns.len() / 100];
-    let p90 = sample_times_ns[90 * sample_times_ns.len() / 100];
-
-    println!("{:>6.2} {:>6.2} {:>6.2} {:>6.2} {:>6.2}",
-             p10, p50, p90, ns_per_sample, bytes_per_sec / 1024.0 / 1024.0);
 }
 
 fn main() {
     let fname = env::args().nth(1).expect("no file given");
 
     let data = read_file(fname);
-    let mut sample_times_ns = Vec::new();
+    let mut sample_times_ns = Vec::with_capacity(10 * 1000);
 
-    // Do a few runs to get more robust statistics.
-    for _ in 0..5 {
-        let stats_pair = decode_file(&data, &mut sample_times_ns);
-        print_stats(&mut sample_times_ns, stats_pair);
+    // Do 10 entire runs to collect more data.
+    for _ in 0..10 {
+        decode_file(&data, &mut sample_times_ns);
+        for ns in &sample_times_ns[..] {
+            println!("{:.5}", ns);
+        }
         sample_times_ns.clear();
     }
+
 }
