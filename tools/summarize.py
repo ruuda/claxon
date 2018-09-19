e84030995bbc629bd7459bedc198b8d8ba2f7555
Ruud van Asseldonk <dev@veniogames.com>
2018-09-19 23:47:21 +0200

    Try to see actual times through the noise
    I want to put proper error bars on my values, but I don't know how to do
    that in this case.
    
    I assume that for a particular block, there is a true decode time T,
    and I can observe T + noise, where the noise is drawn from some
    distribution. It looks steeper than exponential. Perhaps a gamma
    distribution with k < 1.0. So then the question is: given a number of
    observations of T + noise, give the maximum likelihood estimate for T
    and the parameters of the noise distribution. I should work this out
    with pen and paper; now that I am writing it down it seems doable after
    all.

--- tools/summarize.py
+++ tools/summarize.py
@@ -12,25 +12,39 @@ import sys
 
 data = np.genfromtxt(sys.argv[1])
 
-for num_chunks in range(1, 10):
+#data = data[:,0:80]
+for k in range(1, len(data[0]), 10):
+    ds = data[:, 0:k]
+    mins = np.min(ds, axis=1)
+    diffs = np.transpose(ds) - mins
+    mean_noise = np.mean(diffs)
+    print(f'{k:3}: {mean_noise:.3f}')
 
-    # Take the minimum time of all of the recorded times that we have for a block.
-    # This is the time undisturbed by noise.
-    mins = np.min(data, axis=1)
+# Take the minimum time of all of the recorded times that we have for a block.
+# This is the time undisturbed by noise.
+mins = np.min(data, axis=1)
 
-    chunk_mins = np.array([
-        np.min(chunk, axis=1) for chunk in np.array_split(data, num_chunks, axis=1)
-    ])
+# Remote a few outliers from the measurements. These outliers are legitimite
+# measurements (after taking the min), some samples really are that fast to
+# decode. We're just not interested in them. This happens mostly for the few
+# blocks of silence at the beginning or end of a song; these are trivial to
+# decode at less than 1 ns per sample. These samples drive up the variance, so
+# we exclude them.
+outlier_threshold = np.mean(mins) * 0.75
+assert np.quantile(mins, 0.002) > outlier_threshold, 'Unexpected distribution'
+ok_samples = mins[mins > outlier_threshold]
 
-    print(f'{num_chunks} chunks')
-    for q in (0.05, 0.50, 0.95):
-        values = np.quantile(chunk_mins, q, axis=1)
-        mean = np.mean(values)
-        sd = np.std(values)
-        print(f'  q{q:0.2f}: {mean:0.3f} pm {sd:0.03f}')
+diffs = np.transpose(data) - mins
+mean_noise = np.mean(diffs)
 
-q05 = np.quantile(mins, 0.05)
-q50 = np.quantile(mins, 0.50)
-q95 = np.quantile(mins, 0.95)
+qs_noise = -np.log(1.0 - np.array([0.05, 0.50, 0.95])) * mean_noise
+print(f'Noise quantiles if noise were exponential: {qs_noise}')
+print(f'Noise quantiles actual:                    {np.quantile(diffs, (0.05, 0.50, 0.95))}')
 
-print(f'Time per sample: {q50:.3f} ns ({q05:.3f} .. {q95:.3f})')
+error = -np.log(1.0 - (1.0 / len(data[0]))) * mean_noise
+print(f'Error on mean: {error:0.3f}')
+
+
+print(f'removed noise: {np.mean(diffs):6.3f} ns or {np.quantile(diffs, 0.05)}')
+print(f'Time per sample: {np.mean(mins):6.3f} pm {np.std(mins):6.3f}')
+print(f'Time per sample: {np.mean(ok_samples):6.3f} pm {np.std(ok_samples):6.3f}')
