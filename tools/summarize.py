f79fb1893248d2c352738f0c432dd5612b261062
Ruud van Asseldonk <dev@veniogames.com>
2018-09-20 22:11:31 +0200

    Report mean time and confidence intervals
    The confidence interval estimate is a bit ad-hoc, but at least
    empirically it appears to work well. I have a few different runs of the
    benchmark (without changing the program) to judge variance, and all of
    my measurements are within the confidence bounds reported by the others.
    Also, one measurement was taken while my machine was busy, and the
    estimate for the time per sample is very close there, but the confidence
    interval is much wider, so it appears to work.

--- tools/summarize.py
+++ tools/summarize.py
@@ -12,19 +12,18 @@ import sys
 
 data = np.genfromtxt(sys.argv[1])
 
-#data = data[:,0:80]
-for k in range(1, len(data[0]), 10):
-    ds = data[:, 0:k]
-    mins = np.min(ds, axis=1)
-    diffs = np.transpose(ds) - mins
-    mean_noise = np.mean(diffs)
-    print(f'{k:3}: {mean_noise:.3f}')
+# For every decoded block, we have num_samples measurements.
+num_blocks, num_samples = data.shape
 
 # Take the minimum time of all of the recorded times that we have for a block.
-# This is the time undisturbed by noise.
+# This is the time undisturbed by noise, and the time we will report.
 mins = np.min(data, axis=1)
 
-# Remote a few outliers from the measurements. These outliers are legitimite
+# We also report a confidence interval that is based on properties of the noise,
+# and for this we use the means instead.
+means = np.mean(data, axis=1)
+
+# Remove a few outliers from the measurements. These outliers are legitimite
 # measurements (after taking the min), some samples really are that fast to
 # decode. We're just not interested in them. This happens mostly for the few
 # blocks of silence at the beginning or end of a song; these are trivial to
@@ -32,19 +31,40 @@ mins = np.min(data, axis=1)
 # we exclude them.
 outlier_threshold = np.mean(mins) * 0.75
 assert np.quantile(mins, 0.002) > outlier_threshold, 'Unexpected distribution'
-ok_samples = mins[mins > outlier_threshold]
+ok_mins = mins[mins > outlier_threshold]
+ok_means = means[means > outlier_threshold]
 
+# Report the mean time per sample (average the time per sample of the blocks).
+# What we are interested in for optimization purposes is the total running time,
+# not the median or an other quantile, because you always decode all blocks of a
+# file. So we take the mean.
+t_mean = np.mean(ok_mins)
+
+# For every block, we have a number of measurements, that should be of the form
+# t + x, where t is the true time, and x is noise on top. We estimate t as the
+# minimum over all samples. Assuming that the noise follows an exponential
+# distribution, we can compute some additional statistics. NOTE: This assumption
+# does not appear to be valid. The noise has a very particular shape (multiple
+# lobes, and in the first lobe there are small spikes at clear intervals,
+# possibly quantization noise). At a high level it looks more like a gamma
+# distribution with k = 0.5. But we will assume exponential for now because it
+# is easy to work with.
 diffs = np.transpose(data) - mins
 mean_noise = np.mean(diffs)
 
-qs_noise = -np.log(1.0 - np.array([0.05, 0.50, 0.95])) * mean_noise
-print(f'Noise quantiles if noise were exponential: {qs_noise}')
-print(f'Noise quantiles actual:                    {np.quantile(diffs, (0.05, 0.50, 0.95))}')
+# Estimate the bounds of a 95% confidence interval for the parameter lambda of
+# the exponential distribution, and then invert them to get a 95% confidence
+# interval of the expected value of the noise.
+sqrtn = np.sqrt(num_samples)
+noise_bounds = (1.0 + np.array([-1.0, 1.0]) * 1.96 / sqrtn) / mean_noise
+noise_bounds = 1.0 / noise_bounds
 
-error = -np.log(1.0 - (1.0 / len(data[0]))) * mean_noise
-print(f'Error on mean: {error:0.3f}')
+# Recall that we assume times to be of the form t + x where x is noise. We now
+# have a 95% confidence interval for x, and by taking the mean over all timings
+# for a block, we get an estimate of t + x. Combined we get a 95% confidence
+# interval for t. It's a bit of a hack, and there should be a better statistic
+# to report, but for now this will do.
+t_bounds = np.mean(ok_means) - noise_bounds
 
-
-print(f'removed noise: {np.mean(diffs):6.3f} ns or {np.quantile(diffs, 0.05)}')
-print(f'Time per sample: {np.mean(mins):6.3f} pm {np.std(mins):6.3f}')
-print(f'Time per sample: {np.mean(ok_samples):6.3f} pm {np.std(ok_samples):6.3f}')
+print(f'Mean time per sample:    {t_mean:6.3f} ns')
+print(f'95% confidence interval: {t_bounds[0]:6.3f} ns .. {t_bounds[1]:6.3f} ns')
