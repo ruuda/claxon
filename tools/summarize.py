2af6fd8f2c4175a38b7aac10c1ff1da93bb5bd46
Ruud van Asseldonk <dev@veniogames.com>
2018-10-20 19:02:56 +0200

    Structure bench analysis program better
    Also plot the mean time over all blocks, per iteration. I never looked
    at this graph before, but it is interesting: in all of my runs, the
    first iteration is fast, followed by a few very slow ones. After that,
    sometimes the benchmarks stabilize, but sometimes there is a plateau,
    where one of the runs of the benchmark program is much slower than the
    rest.
    
    I think this might explain the fat tail in the noise distribution, so I
    now filter the deltas to be included in the noise analysis to come only
    from the 50% fastest iterations.
    
    The confidence interval computation looks wrong now, but I was going to
    revamp that anyway, so let's not worry about that for now.

--- tools/summarize.py
+++ tools/summarize.py
@@ -9,68 +9,137 @@ Usage:
 
 import numpy as np
 import sys
+from typing import NamedTuple
+
+import matplotlib
+matplotlib.use('gtk3cairo')
 
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt(sys.argv[1])
+class Stats(NamedTuple):
+    # Number of blocks for which we measured the time per sample.
+    num_blocks: int
 
-# For every decoded block, we have num_samples measurements.
-num_blocks, num_samples = data.shape
+    # Number of times we measured each block.
+    num_iters: int
 
-# Take the minimum time of all of the recorded times that we have for a block.
-# This is the time undisturbed by noise, and the time we will report.
-mins = np.min(data, axis=1)
+    # Mean time per sample (in nanoseconds) over all blocks,
+    # one element per iteration.
+    iter_means: np.array
 
-# We also report a confidence interval that is based on properties of the noise,
-# and for this we use the means instead.
-means = np.mean(data, axis=1)
+    # Time above which the iteration is considered an outlier for noise analysis
+    # purposes.
+    iter_outlier_threshold: float
 
-# Remove a few outliers from the measurements. These outliers are legitimite
-# measurements (after taking the min), some samples really are that fast to
-# decode. We're just not interested in them. This happens mostly for the few
-# blocks of silence at the beginning or end of a song; these are trivial to
-# decode at less than 1 ns per sample. These samples drive up the variance, so
-# we exclude them.
-outlier_threshold = np.mean(mins) * 0.75
-assert np.quantile(mins, 0.002) > outlier_threshold, 'Unexpected distribution'
-ok_mins = mins[mins > outlier_threshold]
-ok_means = means[means > outlier_threshold]
+    # Minimum time per sample (in nanoseconds) over all iterations, one element
+    # per block. Excludes outlier blocks that are very fast to decode and
+    # therefore not interesting.
+    block_mins: np.array
 
-# Report the mean time per sample (average the time per sample of the blocks).
-# What we are interested in for optimization purposes is the total running time,
-# not the median or an other quantile, because you always decode all blocks of a
-# file. So we take the mean.
-t_mean = np.mean(ok_mins)
+    # Delays on top of the minimal time per sample for a block. Filtered to
+    # remove outlier iterations.
+    noise: np.array
 
-# For every block, we have a number of measurements, that should be of the form
-# t + x, where t is the true time, and x is noise on top. We estimate t as the
-# minimum over all samples. Assuming that the noise follows an exponential
-# distribution, we can compute some additional statistics. NOTE: This assumption
-# does not appear to be valid. The noise has a very particular shape (multiple
-# lobes, and in the first lobe there are small spikes at clear intervals,
-# possibly quantization noise). At a high level it looks more like a gamma
-# distribution with k = 0.5. But we will assume exponential for now because it
-# is easy to work with. NOTE 2: Actually, the noise distribution loosk a lot
-# like lognormal after removing the zeros.
-diffs = np.transpose(data) - mins
-diffs = np.reshape(diffs, -1)
-diffs = diffs[diffs > 0.0]
-mean_noise = np.mean(diffs)
 
-# TODO: These new bounds look far too tight. What's up?
-conf_bounds = np.array([0.95, 0.95 * 0.5, 0.0])
-qs = -np.log(1.0 - conf_bounds) / num_samples
-noise_offset_bounds = qs * mean_noise * 2.0
+def load(fname) -> Stats:
+    data = np.genfromtxt(sys.argv[1])
 
-# Recall that we assume times to be of the form t + x where x is noise. We now
-# have a 95% confidence interval for x, and by taking the mean over all timings
-# for a block, we get an estimate of t + x. Combined we get a 95% confidence
-# interval for t. It's a bit of a hack, and there should be a better statistic
-# to report, but for now this will do.
-t_bounds = t_mean - noise_offset_bounds
+    # For every decoded block, we have num_samples measurements.
+    num_blocks, num_iters = data.shape
 
-mid_interval = 0.5 * (t_bounds[0] + t_bounds[2])
-plm_interval = 0.5 * (t_bounds[2] - t_bounds[0])
+    # Take the minimum time of all of the recorded times that we have for a
+    # block. This is the time undisturbed by noise, and the time we will report.
+    mins = np.min(data, axis=1)
 
-print(f'Mean time per sample:    {t_mean:6.3f} ns')
-print(f'95% confidence interval: {mid_interval:6.3f} ± {plm_interval:.3f} ns')
+    # Remove a few outliers from the measurements. These outliers are legitimite
+    # measurements (after taking the min), some blocks really are that fast to
+    # decode. We're just not interested in them. This happens mostly for the few
+    # blocks of silence at the beginning or end of a song; these are trivial to
+    # decode at less than 1 ns per sample. These samples drive up the variance,
+    # so we exclude them.
+    outlier_threshold = np.mean(mins) * 0.75
+    assert np.quantile(mins, 0.002) > outlier_threshold, 'Unexpected distribution'
+    ok_mins = mins[mins > outlier_threshold]
+
+    # For analysis of the noise, we also want to remove outliers, but this time
+    # outliers where the entire iteration was slow. This can happen; especially
+    # the first few iterations can be much slower than later ones, but then
+    # still, sometimes there are plateaus where one run of the benchmark program
+    # (which spans multiple iterations) is much slower than others. Perhaps it
+    # got unlucky with ASLR and that caused more TLB misses? Single iterations
+    # can be unexpectedly slow too, perhaps because some daemon had to do a
+    # periodic task. Whatever the cause, for the purpose of analyzing the noise
+    # distribution, these severe outliers cause a fat tail, so we exclude those
+    # iterations entirely. They are still used for the minimum time per block,
+    # just not for analyzing the noise.
+    iter_means = np.mean(data, axis=0)
+    iter_outlier_threshold = np.quantile(iter_means, 0.5)
+
+    # For every block, we have a number of measurements, that should be of the
+    # form t + x, where t is the true time, and x is noise on top. We estimate t
+    # as the minimum over all samples. Then for every iteration that was below
+    # the threshold, we get the noise delay x, and by collecting all of the
+    # delays we can quantify the noise and report its properties.
+    # NOTE: Looks lognormal.
+    diffs = np.transpose(data) - mins
+    ok_diffs = diffs[iter_means < iter_outlier_threshold]
+
+    # We now have the noise per block, but the particular block is not that
+    # interesting, so flatten the matrix. Also exclude zeros, those are not
+    # noise.
+    noise = np.reshape(ok_diffs, -1)
+    noise = noise[noise > 0.0]
+
+    return Stats(
+        num_blocks = num_blocks,
+        num_iters = num_iters,
+        iter_means = iter_means,
+        iter_outlier_threshold = iter_outlier_threshold,
+        block_mins = ok_mins,
+        noise = noise,
+    )
+
+
+def plot(stats: Stats) -> None:
+    plt.plot(np.arange(0, stats.num_iters), stats.iter_means)
+    plt.axhline(np.mean(stats.iter_outlier_threshold))
+    plt.show()
+
+
+def report(stats: Stats) -> None:
+    # Report the mean time per sample (average the time per sample over the
+    # blocks). What we are interested in for optimization purposes is the total
+    # running time, not the median or an other quantile, because you always
+    # decode all blocks of a file. So we take the mean.
+    t_mean = np.mean(stats.block_mins)
+
+    mean_noise = np.mean(stats.noise)
+    print(mean_noise)
+
+    # TODO: These new bounds look far too tight. What's up?
+    conf_bounds = np.array([0.95, 0.95 * 0.5, 0.0])
+    qs = -np.log(1.0 - conf_bounds) / stats.num_blocks
+    noise_offset_bounds = qs * mean_noise
+
+    # Recall that we assume times to be of the form t + x where x is noise. We
+    # now have a 95% confidence interval for x, and by taking the mean over all
+    # timings for a block, we get an estimate of t + x. Combined we get a 95%
+    # confidence interval for t. It's a bit of a hack, and there should be a
+    # better statistic to report, but for now this will do.
+    t_bounds = t_mean - noise_offset_bounds
+
+    mid_interval = 0.5 * (t_bounds[0] + t_bounds[2])
+    plm_interval = 0.5 * (t_bounds[2] - t_bounds[0])
+
+    print(f'Mean time per sample:    {t_mean:6.3f} ns')
+    print(f'95% confidence interval: {mid_interval:6.3f} ± {plm_interval:.3f} ns')
+
+
+if len(sys.argv) < 2:
+    print(__doc__)
+    sys.exit(1)
+
+for fname in sys.argv[1:]:
+    stats = load(fname)
+    plot(stats)
+    report(stats)
